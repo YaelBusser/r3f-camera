ae06460387581ef26929140695a8bb4957d4e488
Yaël Busser <mrmaclevine@gmail.com>
2024-11-21 15:07:51 +0100

    finish course

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 import './App.css'
 import {Canvas} from "@react-three/fiber";
-import {OrbitControls} from "@react-three/drei";
+import {Environment, OrbitControls, OrthographicCamera, PerspectiveCamera} from "@react-three/drei";
+import {Model as City} from "./components/models/City.tsx"
+import * as THREE from "three";
 
 function App() {
     return (
-        <Canvas camera={{
-            position: [3, 3, 3]
-        }}>
-            <mesh>
-                <OrbitControls/>
-                <boxGeometry/>
-                <meshNormalMaterial/>
-            </mesh>
+        <Canvas>
+            {
+                /*
+                TOUTES LES CAMERAS :
+                   - Controler la caméra simplement
+                        <OrbitControls/>
+                   - Caméra principale (qu'on voit avec nos yeux)
+                        <Canvas camera={{position: []}} == PerspectiveCamera avec makeDefault
+                        <PerspectiveCamera position={[3, 3, 3]} makeDefault fov={60} near={0.1} far={15}/>
+                   - Caméra sans perspective (ex: jeu vidéo comme warcraft), tous les objets ont la même dimension
+                 */
+            }
+            <OrthographicCamera position={[1, 1, 1]} makeDefault top={1.5} bottom={-2}
+                                right={2 * (window.innerWidth / window.innerHeight)}
+                                left={-2 * (window.innerWidth / window.innerHeight)}
+                                near={-5}
+            />
+            <OrbitControls/>
+            <City/>
+            <Environment preset={"city"}/>
         </Canvas>
     )
 }
