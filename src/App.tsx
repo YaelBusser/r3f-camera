6310900ca4f16587a0a30733b2eaa739a275d4be
Yaël Busser <mrmaclevine@gmail.com>
2024-11-21 15:09:36 +0100

    camera course done

--- src/App.tsx
+++ src/App.tsx
@@ -16,13 +16,13 @@ function App() {
                         <Canvas camera={{position: []}} == PerspectiveCamera avec makeDefault
                         <PerspectiveCamera position={[3, 3, 3]} makeDefault fov={60} near={0.1} far={15}/>
                    - Caméra sans perspective (ex: jeu vidéo comme warcraft), tous les objets ont la même dimension
+                        <OrthographicCamera position={[1, 1, 1]} makeDefault top={1.5} bottom={-2}
+                                            right={2 * (window.innerWidth / window.innerHeight)}
+                                            left={-2 * (window.innerWidth / window.innerHeight)}
+                                            near={-5}
+                        />
                  */
             }
-            <OrthographicCamera position={[1, 1, 1]} makeDefault top={1.5} bottom={-2}
-                                right={2 * (window.innerWidth / window.innerHeight)}
-                                left={-2 * (window.innerWidth / window.innerHeight)}
-                                near={-5}
-            />
             <OrbitControls/>
             <City/>
             <Environment preset={"city"}/>
